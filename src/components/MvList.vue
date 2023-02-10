5d32ccaa3840ff010dc2b099f57f91c6132bb5e1
penghui.yang <penghui.yang@mfashion.com.cn>
2023-02-10 09:47:56 +0800

    完成部分功能，提交预览

---
+++ src/components/MvList.vue
@@ -0,0 +1,198 @@
+<template>
+    <el-skeleton
+        :loading="loading"
+        animated
+        :count="num"
+        :throttle="500"
+        >
+        <template #template>
+            <div class="item">
+                <el-skeleton-item class="ske-img" variant="h3" />
+                <div class="ske-info">
+                    <el-skeleton-item variant="h3" class="ske-name" />
+                    <el-skeleton-item variant="text" class="ske-txt" style="width:50%" />
+                    <el-skeleton-item variant="text" class="ske-txt" />
+                </div>
+            </div>
+        </template>
+        <template #default>
+            <div class='mv'>
+                <!-- 列表有重复的MV， 相同id的key会报错，加上字符串的index -->
+                <router-link :to="{ path: '/mvlist/mv', query: { id: item.id }}" class="item" :key="'' + item.id + index" v-for="(item, index) in mvList">
+                    <div class="faceImg">
+                        <i class="iconfont icon-mvlist"></i>
+                        <el-image :src="item.cover || item.imgurl">
+                            <div slot="placeholder" class="image-slot">
+                                <i class="iconfont icon-placeholder"></i>
+                            </div>
+                        </el-image>
+                    </div>
+                    <div class="info">
+                        <div class="mv-name">{{item.name}}</div>
+                        <div class="mv-author" v-if="!item.publishTime">{{item.artistName}}</div>
+                        <div class="mv-playCount"><i class="iconfont icon-mvlist"></i> {{$utils.formartNum(item.playCount)}}</div>
+                        <div class="mv-time" v-if="item.publishTime">发布时间：{{item.publishTime}}</div>
+                    </div>
+                </router-link>
+            </div>
+        </template>
+    </el-skeleton>
+</template>
+
+<script>
+export default {
+    props: {
+        mvList: Array,
+        num: Number,
+        loading: Boolean
+    },
+    setup() {
+    }
+}
+</script>
+<style scoped lang="less">
+.mv {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: left;
+    padding-bottom: 20px;
+    margin: 0 -10px;
+    font-size: 0;
+
+    .item {
+        flex: 30%;
+        max-width: calc(100% / 5 - 20px);
+        margin: 20px 10px 0;
+        border-radius: 4px;
+
+        &:hover {
+            text-decoration: none;
+            .el-image {
+                transform: scale(1.1);
+            }
+
+            .icon-mvlist {
+                opacity: 1;
+                transform: scale(1);
+            }
+        }
+    }
+
+    .faceImg {
+        display: block;
+        position: relative;
+        width: 100%;
+        height: 123px;
+        overflow: hidden;
+        border-radius: var(--border-radius);
+
+        .icon-mvlist {
+            position: absolute;
+            top: 45%;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            z-index: 3;
+            margin-top: -30px;
+            font-size: 60px;
+            text-align: center;
+            color: #fff;
+            opacity: 0;
+            transform: scale(.5);
+            transition: all .4s linear;
+            text-shadow: 2px 2px 10px #000;
+        }
+    }
+
+    .el-image {
+        display:flex;
+        height: 100%;
+        justify-content: center;
+        align-items: center;
+        z-index: 2;
+        transition: all .4s linear;
+
+        :deep(img) {
+            height: auto;
+        }
+    }
+
+    .info {
+        position: relative;
+        padding: 15px 0 0;
+
+        .mv-name {
+            display: block;
+            font-size: 14px;
+            height: 20px;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-line-clamp: 1;
+            -webkit-box-orient: vertical;
+            word-break: break-all;
+            color: var(--t-muted-color);
+        }
+
+        .mv-author, .mv-playCount, .mv-time {
+            display: block;
+            line-height: 24px;
+            font-size: 14px;
+            font-weight: 300;
+            color: var(--t-muted-color);
+        }
+
+        .icon-mvlist {
+            display: inline-block;
+            font-size: 18px;
+            color: var(--t-light-color);
+            vertical-align: top;
+        }
+    }
+}
+
+.el-skeleton {
+    display: flex;
+    width: auto;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: left;
+    margin: 0 -10px;
+    font-size: 0;
+
+    .item {
+        flex: 30%;
+        max-width: calc(100% / 5 - 20px);
+        margin: 20px 10px 0;
+        border-radius: 4px;
+    }
+
+    .ske-img {
+        display: block;
+        position: relative;
+        width: 100%;
+        height: 123px;
+        overflow: hidden;
+    }
+
+    .ske-info {
+        position: relative;
+        padding: 15px 0;
+    }
+
+    .ske-name {
+        display: block;
+        margin: 0 0 6px;
+        line-height: 14px;
+        height: 14px;
+    }
+
+    .ske-txt {
+        display: block;
+        margin: 0 0 8px 0;
+        line-height: 16px;
+        width: 10%;
+    }
+}
+</style>
---
+++ src/store/playerSetting.js
@@ -0,0 +1,33 @@
+import { defineStore } from 'pinia';
+
+const playerSettingInfo = JSON.parse(window.localStorage.getItem('player-setting')) || {};
+
+export const playerSettingStore = defineStore('playerSetting', {
+    state: () => {
+        return {
+            "mode": playerSettingInfo.mode || 0,       // 播放模式  0循环播放、1单曲循环、2随机播放
+            "volume": playerSettingInfo.volume ?? .8,    // 播放音量
+            "lock": playerSettingInfo.lock || false,    // 播放条是否固定
+        }
+    },
+    getters: {
+        getMode: (state) => {
+            return state.mode || 0;
+        },
+        getVolume: (state) => {
+            return state.volume ?? .8;
+        },
+        getLock: (state) => {
+            return state.lock || false;
+        },
+    },
+    actions: {
+        setPlayerSetting(type, val) {
+            const info = JSON.parse(window.localStorage.getItem('player-setting')) || {};
+
+            info[type] = val;
+
+            window.localStorage.setItem('player-setting', JSON.stringify(info));
+        }
+    }
+});
